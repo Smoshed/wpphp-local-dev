03d7ca21ee652448fd915bb67528af63bc2d0204
Smoshed <j.a.oltmans@gmail.com>
2024-11-25 15:20:55 +0100

    Update functions.php

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -18,9 +18,18 @@ add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query) {
   if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $today = date('Ymd');
     $query->set('meta_key', 'even_date');
     $query->set('orderby', 'meta_value_num');
-    $query->ser('order', 'ASC');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+      array(
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => $today,
+        'type' => 'numeric'
+      )
+    ));
   }
 }
 
