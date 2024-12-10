7007fe4a48bd1984304e09a6671917fc2ebadcf1
Smoshed <j.a.oltmans@gmail.com>
2024-12-10 13:23:22 +0100

    ertfjgbne

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,4 +1,19 @@
 <?php
+
+function pageBanner() {
+  // php logic will live here
+  ?>
+  <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p><?php the_field('page_banner_subtitle') ?></p>
+        </div>
+      </div>  
+    </div>
+<?php }
+
 function university_files() {
   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -14,6 +29,7 @@ function university_features() {
   add_theme_support('post-thumbnails');
   add_image_size('professorLandscape', 400, 260, true);
   add_image_size('prefessorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
